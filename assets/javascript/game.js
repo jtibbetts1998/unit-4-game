ece2f22d2ad46602776512a5950e3991b7765c32
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-11-14 12:11:43 -0500

    Click buttom funtional and addition to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,18 @@
-// Global variables that hook into html document
+console.log("Hi")
+// Making scoreboard
 
-    var
-    var
-    var
-    var
-    var
+    var wins = 0;
+    var losses = 0;
+    var score = 0;
 
+    var winsText = document.getElementById("wins");
+    var lossesText = document.getElementById("losses");
+    var scoreText = document.getElementById("score");
+
+    // funtion reset() {
+
+    // }
+
+    $(".crystal").on("click", function() {
+        console.log("Hi")
+    });
